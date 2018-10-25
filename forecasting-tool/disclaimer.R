8d6c127380cb7971b2b96591fcf5197831d85add
Sam Supplee-Niederman <niederman.sam@gmail.com>
2018-10-24 21:45:07 -0600

    forecasting app

---
+++ forecasting-tool/disclaimer.R
@@ -0,0 +1,30 @@
+
+disclaimer <-  tags$p(
+        "For more details about the mechanics and mathmatics behind these forecasting
+        techniques, check out the resources below:",
+        tags$br(), tags$br(),
+
+      tags$a(
+        "Forecasting Principles and Practice",
+        href="https://otexts.org/fpp2/",
+        target="_blank"),
+
+      tags$br(),
+
+      tags$a("Introduction to ARIMA Forecasting",
+             href='https://www.datascience.com/blog/introduction-to-forecasting-with-arima-in-r-learn-data-science-tutorials',
+             target="_blank"),
+
+      tags$br(),
+
+      tags$a("Intro to Time Series Decomposition",
+             href='https://align-alytics.com/seasonal-decomposition-of-time-series-by-loessan-experiment/',
+             target="_blank"),
+
+      tags$br(), tags$br(),
+
+      tags$strong("Disclaimer:"),tags$br(), "This forecasting application is
+      intended for demostration and education. No holdback sample was used.
+      Statistical models cannot account for dramatic changes in market
+      conditions, internal dynamics, and/or acts of the divine."
+    , tags$br())
